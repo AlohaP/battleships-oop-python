d7f2de0d108bd0d994c42e35426774cfbe0c69b0
Adam Kruk <adam.kruk91@gmail.com>
2017-10-25 13:15:04 +0200

    After merge

--- square.py
+++ square.py
@@ -18,14 +18,10 @@ class Square():
         self.sign = 'O'
 
     def change_to_hit(self):
-<<<<<<< HEAD
         if self.sing == "@":
             self.sign = 'X'
         else:
             self.sign = 'O'
-=======
-        self.sign = 'X'
 
     def change_to_sunk(self):
         self.sunk = True
->>>>>>> 4c9caddefdc5e46e4dcc6f13680730322b235f07
