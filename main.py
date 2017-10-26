0604e7f050fbb1569b66ee0c17d0be4b2562e1d0
Sebastian Slupinski <slupinski.sebastian@gmail.com>
2017-10-26 09:59:19 +0200

    add player vs player in menu

--- main.py
+++ main.py
@@ -1,27 +1,51 @@
 import game
+from square import Square
+from player import Player
+from game import PlayBattleships
+from cpu import PlayBattleshipsWithCPU
+from ship import Ship
+from ocean import Ocean
 
 
-def high_score():
-    print("elo")
+menu_commands = ["Player vs Player", "Player vs CPU", "How to play",
+                "Hall of Fame", "Exit"] 
 
-def instruction():
-    print("elo")
 
+def print_program_menu(menu_commands):
+
+    for option in menu_commands:
+        print(str(menu_commands.index(option)) + "----->" + option)
 
 
 def main():
+
     while True:
-        menu_select = input("""
-                            1. Player vs CPU
-                            2. Player vs Player
-                            3. Hall of Fame
-                            4. How to play
-                            0. Quit
-                            """)
-        menu = {"0": quit, "1": game.create_player_ships, "2": game.player_vs_player, "3": high_score, "4": instruction}
-        if menu_select not in menu:
-            continue
-        menu[menu_select]()
+
+        print_program_menu(menu_commands)
+
+        option = input("Select an option: ")
+
+        if option == "0":
+
+            player1 = Player(input("Please enter your name: "))
+            player2 = Player(input("Please enter player 2 name: "))
+
+            game = PlayBattleships(player1, player2)
+            game.boards_setup(player1, player2)
+
+            while True:
+                game.turn_mechanics(player1, player2)
+                game.turn_mechanics(player2, player1)
+            
+
+
+def high_score():
+
+    print("elo")
+
+def instruction():
+
+    print("elo")
 
 if __name__ == "__main__":
     main()
